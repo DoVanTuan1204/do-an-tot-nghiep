b268b6a18c549a3c9f87fbf36fe828554077d5c6
DoVanTuan1204 <dovantuan1404@gmail.com>
2023-06-09 01:22:44 +0700

    change logo

--- models/Category.js
+++ models/Category.js
@@ -1,9 +1,9 @@
-import mongoose, {model, models, Schema} from "mongoose";
+import mongoose, { model, models, Schema } from "mongoose";
 
 const CategorySchema = new Schema({
-  name: {type:String,required:true},
-  parent: {type:mongoose.Types.ObjectId, ref:'Category'},
-  properties: [{type:Object}]
+  name: { type: String, required: true },
+  parent: { type: mongoose.Types.ObjectId, ref: "Category" },
+  properties: [{ type: Object, default: null }],
 });
 
-export const Category = models?.Category || model('Category', CategorySchema);
+export const Category = models?.Category || model("Category", CategorySchema);
